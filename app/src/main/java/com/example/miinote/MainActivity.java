3ba7fe543f7b38c89528e1bae0838eb55bd59b1b
Christianbeny <s16120301@alimnos.itsur.edu.mx>
2020-11-08 17:24:42 -0600

    arreglar boton

--- app/src/main/java/com/example/miinote/MainActivity.java
+++ app/src/main/java/com/example/miinote/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.agregar);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
