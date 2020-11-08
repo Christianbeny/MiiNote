04bd6807bd0e38c742d6461e8eaa995a52231035
Christianbeny <s16120301@alimnos.itsur.edu.mx>
2020-11-08 17:48:10 -0600

    arreglo

--- app/src/main/java/com/example/miinote/MainActivity.java
+++ app/src/main/java/com/example/miinote/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.agregar);
+        FloatingActionButton fab = findViewById(R.id.agrega);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
