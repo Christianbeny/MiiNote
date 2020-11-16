6834bb8ab18fe5408fca822a5be43d026e45fc8e
ManuelZamudioPaniagua <s17120266@alumnos.itsur.edu.mx>
2020-11-16 14:49:37 -0600

    Correcion del modo Landscape

--- app/src/main/java/com/example/miinote/FirstFragment.java
+++ app/src/main/java/com/example/miinote/FirstFragment.java
@@ -19,16 +19,4 @@ public class FirstFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-    }
 }
--- app/src/main/java/com/example/miinote/LlenadoNota.java
+++ app/src/main/java/com/example/miinote/LlenadoNota.java
@@ -3,12 +3,22 @@ package com.example.miinote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LlenadoNota extends AppCompatActivity {
+
+    private EditText tiutulo;
+    private EditText descripcion;
+    private EditText nota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_llenado_nota);
+
+        tiutulo = (EditText)findViewById(R.id.txt_Titulo);
+        descripcion = (EditText)findViewById(R.id.txt_Descripcion);
+        nota = (EditText)findViewById(R.id.txt_Texto);
+
     }
 }
