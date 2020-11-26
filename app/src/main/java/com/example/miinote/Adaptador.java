db101058f5eedc6d18a3127efd9c763a5a3d85f4
ManuelZamudioPaniagua <s17120266@alumnos.itsur.edu.mx>
2020-11-26 14:55:38 -0600

    Se añadio clase Adaptador

---
+++ app/src/main/java/com/example/miinote/Adaptador.java
@@ -0,0 +1,34 @@
+package com.example.miinote;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class Adaptador extends BaseAdapter {
+//78784
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+
+    //
+
+
+}
